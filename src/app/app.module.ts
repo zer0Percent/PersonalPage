1db608a7e106c28c6e04d226967115b1f2204f19
zer0Percent <dominguezcalvoalvaro@gmail.com>
2019-12-07 16:47:49 +0100

    View form contact done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,28 @@ import { FooterComponent } from './components/footer/footer.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import { FormEmailComponent } from './components/form-email/form-email.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
+    FormEmailComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+   MatFormFieldModule,
+   MatSelectModule,
+   MatInputModule,
+   MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/form-email/form-email.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'formEmail',
+  templateUrl: './form-email.component.html',
+  styleUrls: ['./form-email.component.css']
+})
+export class FormEmailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
